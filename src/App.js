ba2086c81ded0baa1edc10e94e50f909ad450a56
Tyler Ramsey <tyler.ramsey@leadingedje.com>
2022-10-05 12:14:19 -0400

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import './App.css';
+import About from './About';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Router>
+          <Routes>
+            <Route exact path="/about" element={<About/>}/>
+          </Routes>
+      </Router>
+      </header>
+    </div>
+  );
+}
+
+export default App;
