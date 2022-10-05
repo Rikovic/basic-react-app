cdb9d19695768a6240748a2684360b3f093f5c96
Tyler Ramsey <tyler.ramsey@leadingedje.com>
2022-10-05 12:27:38 -0400

    Change test.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is a test <code>src/App.js</code> and save to reload.
         </p>
         <a href="/about">About Test Page</a>
         <Router>
