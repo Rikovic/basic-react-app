25070ed2d1df5ade0d6efcdbda33ef09099f69b1
Tyler Ramsey <tyler.ramsey@leadingedje.com>
2022-10-05 12:23:11 -0400

    Adding about link

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <a href="/about">About Test Page</a>
         <Router>
           <Routes>
             <Route exact path="/about" element={<About/>}/>
